a40e8b245c87902acad4f7426f444fbf234c3e7d
Vidhu Prakash T P <vidhuprakashchr1@gmail.com>
2023-08-22 21:14:15 +0530

    Form validation compleated and bugs fixed

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 
-function ShowError(error){
-    document.getElementsByClassName('return-error')[0].innerHTML=error;
-}
-
 function ValidateForm(){
-    var return_value = true; 
-
+ var val=true;
  var name = document.forms['MyForm']["name"].value;
  var phonenumber = document.forms['MyForm']["number"].value;
  var email = document.forms['MyForm']["email"].value;
  var password = document.forms['MyForm']["pass"].value;
  var cpassword = document.forms['MyForm']["cpass"].value;
+ var nameERROR= document.getElementById("name-error");
+ var numERROR= document.getElementById("num-error");
+ var emailERROR= document.getElementById("email-error");
+ var passERROR=document.getElementById("pass-error");
+ var cpassERROR=document.getElementById("cpass-error");
+ var usr=document.getElementById("usr");
+ nameERROR.innerHTML="";
+ numERROR.innerHTML="";
+ emailERROR.innerHTML="";
+ passERROR.innerHTML="";
+ cpassERROR.innerHTML="";
  if(name.length>10){
-    ShowError("*Username is too long");
-    return false;
+    nameERROR.innerHTML="Username is greater than 10";
+    val=false;
  }
- if(name==""){
-    ShowError("*Please enter username without fail");
-    return false;
+ if(name.length<5){
+    nameERROR.innerHTML="Username is smaller than 5";
+    val=false;
  }
- else{
-    return true;
+ if(phonenumber.length!=10||isNaN(phonenumber)){
+    console.log("invalid number");
+    numERROR.innerHTML="Invalid number";
+    val=false;
  }
+ if(email.length<2) {
+    emailERROR.innerHTML = 'Invalid email address';
+    val = false;
+}
+if(password<8){
+    passERROR.innerHTML = "Must contain more than 8 values";
+    val = false;
+}
+if(cpassword==""||password!=cpassword){
+    cpassERROR.innerHTML="Password does not match";
+    val=false;
+}
+
+
+    return val;
 }
